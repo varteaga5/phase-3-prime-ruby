bab20dbed9e05540479003e41f7a77679248e726
vincent <vincent.arteaga@gmail.com>
2021-07-22 13:59:44 -0700

    lab complete

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime? (num)
+    return false if num <= 1
+    for i in (2..num-1)
+        if num % i == 0 && num != i
+            return false
+        end    
+    end 
+    return true
+end
